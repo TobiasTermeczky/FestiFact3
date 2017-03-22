944be004f20999df0adc077d5f017874a5ae2ce8
Tobias <tobiastermeczky@gmail.com>
2017-03-22 14:39:33 +0100

    Init FestiFact3 setup with working roles.

---
+++ FestiFact3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FestiFact3.MvcApplication" Language="C#" %>
---
+++ FestiFact3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FestiFact3.Startup))]
+namespace FestiFact3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
